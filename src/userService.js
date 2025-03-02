eb8d85c0c8426bad0716a08262124d9422a33c50
Mettelund97 <lu.nd3@hotmail.com>
2025-03-02 14:43:33 +0100

    hej

---
+++ src/userService.js
@@ -0,0 +1,27 @@
+function getUser(id) {
+    var user = undefined;
+    if (id == null) return;
+
+    if (id == 1) {
+        user = { name: "John", age: "25" };
+    } else if (id == 2) {
+        user = { name: "Jane", age: "24" };
+    }
+
+    return user;
+}
+function saveUser(userData) {
+    // TODO: Implement data validation
+    const user_data = userData;
+    var success = true;
+
+    try {
+        // Simulating database save
+        console.log("Saving user:", user_data);
+    } catch (error) {
+        success = false;
+    }
+
+    return success;
+}
+module.exports = { getUser, saveUser };
